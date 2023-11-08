18258d31bcd2cb9b6ebbfb7a25fef5b73bbf1ce9
Ragudos <workemailaaronragudos@gmail.com>
2023-11-09 00:16:41 +0800

    more fixinggg

--- netlify/functions/server.cjs
+++ netlify/functions/server.cjs
@@ -42246,6 +42246,7 @@ var import_express2 = __toESM(require_dist3());
 var import_node_path = __toESM(require("node:path"));
 function getServer() {
   const build_dir = import_node_path.default.join(process.cwd(), "..", "..", "build");
+  console.log(build_dir);
   for (const key in require.cache) {
     if (key.startsWith(build_dir)) {
       delete require.cache[key];
--- server/get-server.ts
+++ server/get-server.ts
@@ -3,6 +3,8 @@ import path from "node:path";
 export function getServer() {
     const build_dir = path.join(process.cwd(), "..", "..", "build");
 
+    console.log(build_dir);
+
     for (const key in require.cache) {
         if (key.startsWith(build_dir)) {
             delete require.cache[key];
