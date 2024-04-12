cd73208bc256f397ba318ea43c295e4fe2985c39
Aaron Ragudos <workemailaaronragudos@gmail.com>
2024-04-13 00:44:32 +0800

    try serverless http module for deployment

--- server.ts
+++ server.ts
@@ -1,9 +1,9 @@
 import { app } from "server/init";
 import { loadAssets } from "server/load-assets";
-import { startServer } from "server/start-server";
+import serverless from "serverless-http";
 
 const PORT = +(process.env.PORT || 3000);
 
 app.all("*", loadAssets);
 
-startServer(PORT);
+export const handler = serverless(app);
